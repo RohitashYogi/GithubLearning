10ac590884336a5056e5d5baeecb7f2cf72cad51
RohitashYogi <rohitash.yogi@probusinsurance.com>
2021-09-22 16:47:55 +0530

    Bottom Sheet  Libaray

--- app/src/main/java/com/TechSoft/githublearning/MainActivity.java
+++ app/src/main/java/com/TechSoft/githublearning/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.techsoft.firstlibarary.Toaster;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toaster.SimpleToastCenter(getApplicationContext(),"Center Wal Toast");
-        Toaster.SimpleToast(getApplicationContext(),"simple Wal Toast");
+
     }
 }
